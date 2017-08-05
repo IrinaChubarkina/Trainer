1589e7ebd5b39e4c2c324b3746dae00047b43c28
Nikita Bonachev <nbonachev@gmail.com>
2017-08-05 20:47:14 +0500

    add v-for

--- client/index.js
+++ client/index.js
@@ -3,6 +3,26 @@ import 'styles/style.scss';
 const app = new Vue({
     el: '#app',
     data: {
-        title: 'Weather'
+        title: 'Weather',
+        cities: [
+            {
+                name: 'Пермь',
+                temp: '+12',
+                condition: 'Clear',
+                icon: '//cdn.apixu.com/weather/64x64/night/113.png'
+            },
+            {
+                name: 'Москва',
+                temp: '+15',
+                condition: 'Partly cloudy',
+                icon: '//cdn.apixu.com/weather/64x64/day/116.png'
+            },
+            {
+                name: 'Санкт-Петербург',
+                temp: '+12',
+                condition: 'Light rain shower',
+                icon: '//cdn.apixu.com/weather/64x64/day/353.png'
+            },
+        ]
     }
 });
